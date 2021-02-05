3c7c02a576fde51af300a374d459d3cc833e412d
Igor Cappello <igor.cappello@gmail.com>
2021-02-05 11:16:48 +0100

    Adding support for DATA attachments

--- test/lib/adapters/sendin_blue_adapter_test.exs
+++ test/lib/adapters/sendin_blue_adapter_test.exs
@@ -1,6 +1,7 @@
 defmodule Bamboo.SendinBlueAdapterTest do
   use ExUnit.Case
   alias Bamboo.Email
+  alias Bamboo.Attachment
   alias Bamboo.SendinBlueAdapter
 
   @config %{adapter: SendinBlueAdapter, api_key: "123_abc"}
@@ -9,20 +10,22 @@ defmodule Bamboo.SendinBlueAdapterTest do
   defmodule FakeSendinBlue do
     use Plug.Router
 
-    plug Plug.Parsers,
+    plug(Plug.Parsers,
       # parsers: [:urlencoded, :multipart, :json],
       parsers: [:multipart, :json],
       pass: ["*/*"],
       json_decoder: Poison
-    plug :match
-    plug :dispatch
+    )
+
+    plug(:match)
+    plug(:dispatch)
 
     def start_server(parent) do
-      Agent.start_link(fn -> Map.new end, name: __MODULE__)
+      Agent.start_link(fn -> Map.new() end, name: __MODULE__)
       Agent.update(__MODULE__, &Map.put(&1, :parent, parent))
       port = get_free_port()
       Application.put_env(:bamboo, :sendinblue_base_uri, "http://localhost:#{port}")
-      Plug.Adapters.Cowboy.http __MODULE__, [], port: port, ref: __MODULE__
+      Plug.Adapters.Cowboy.http(__MODULE__, [], port: port, ref: __MODULE__)
     end
 
     defp get_free_port do
@@ -33,7 +36,7 @@ defmodule Bamboo.SendinBlueAdapterTest do
     end
 
     def shutdown do
-      Plug.Adapters.Cowboy.shutdown __MODULE__
+      Plug.Adapters.Cowboy.shutdown(__MODULE__)
     end
 
     post "/v2.0/email" do
@@ -44,8 +47,8 @@ defmodule Bamboo.SendinBlueAdapterTest do
     end
 
     defp send_to_parent(conn) do
-      parent = Agent.get(__MODULE__, fn(set) -> Map.get(set, :parent) end)
-      send parent, {:fake_sendinblue, conn}
+      parent = Agent.get(__MODULE__, fn set -> Map.get(set, :parent) end)
+      send(parent, {:fake_sendinblue, conn})
       conn
     end
   end
@@ -53,9 +56,9 @@ defmodule Bamboo.SendinBlueAdapterTest do
   setup do
     FakeSendinBlue.start_server(self())
 
-    on_exit fn ->
-      FakeSendinBlue.shutdown
-    end
+    on_exit(fn ->
+      FakeSendinBlue.shutdown()
+    end)
 
     :ok
   end
@@ -88,13 +91,14 @@ defmodule Bamboo.SendinBlueAdapterTest do
   end
 
   test "deliver/2 sends from, html and text body, subject, and headers" do
-    email = new_email(
-      from: {"From", "from@foo.com"},
-      subject: "My Subject",
-      text_body: "TEXT BODY",
-      html_body: "HTML BODY",
-    )
-    |> Email.put_header("Reply-To", {"ReplyTo", "reply@foo.com"})
+    email =
+      new_email(
+        from: {"From", "from@foo.com"},
+        subject: "My Subject",
+        text_body: "TEXT BODY",
+        html_body: "HTML BODY"
+      )
+      |> Email.put_header("Reply-To", {"ReplyTo", "reply@foo.com"})
 
     email |> SendinBlueAdapter.deliver(@config)
 
@@ -109,11 +113,12 @@ defmodule Bamboo.SendinBlueAdapterTest do
   end
 
   test "deliver/2 correctly formats recipients" do
-    email = new_email(
-      to: [{"ToName", "to@bar.com"}, {nil, "noname@bar.com"}],
-      cc: [{"CC", "cc@bar.com"}],
-      bcc: [{"BCC", "bcc@bar.com"}],
-    )
+    email =
+      new_email(
+        to: [{"ToName", "to@bar.com"}, {nil, "noname@bar.com"}],
+        cc: [{"CC", "cc@bar.com"}],
+        bcc: [{"BCC", "bcc@bar.com"}]
+      )
 
     email |> SendinBlueAdapter.deliver(@config)
 
@@ -123,8 +128,37 @@ defmodule Bamboo.SendinBlueAdapterTest do
     assert params["bcc"] == %{"bcc@bar.com" => "BCC"}
   end
 
+  test "deliver/2 correctly formats DATA attachments" do
+    email =
+      new_email(
+        to: "noname@bar.com",
+        attachments: [
+          %Attachment{
+            data: File.read!("./test/support/attachment.png"),
+            content_type: "image/png",
+            filename: Path.basename("attachment1.png")
+          },
+          %Attachment{
+            data: File.read!("./test/support/attachment.png"),
+            content_type: "image/png",
+            filename: Path.basename("attachment2.png")
+          }
+        ]
+      )
+
+    email |> SendinBlueAdapter.deliver(@config)
+    assert_receive {:fake_sendinblue, %{params: params}}
+    assert params["to"] == %{"noname@bar.com" => ""}
+
+    assert params |> get_in(["attachment", "attachment1.png"]) ==
+             File.read!("./test/support/attachment.png") |> Base.encode64()
+
+    assert params |> get_in(["attachment", "attachment2.png"]) ==
+             File.read!("./test/support/attachment.png") |> Base.encode64()
+  end
+
   defp new_email(attrs \\ []) do
     attrs = Keyword.merge([from: "foo@bar.com", to: []], attrs)
-    Email.new_email(attrs) |> Bamboo.Mailer.normalize_addresses
+    attrs |> Email.new_email() |> Bamboo.Mailer.normalize_addresses()
   end
 end

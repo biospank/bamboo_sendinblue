2c0891c15be0fa05e0499e552271a0b272bcfbe7
icappello <icappello@users.noreply.github.com>
2021-02-11 19:47:04 +0100

    Fixes Multiple Recipients support, adds data attachment support (#4)
    * Add support for attachments and fiix format for multiple recipients
    
    Co-authored-by: Igor Cappello <icappello@coders51.com>

--- lib/bamboo/adapters/sendin_blue_adapter.ex
+++ lib/bamboo/adapters/sendin_blue_adapter.ex
@@ -28,6 +28,7 @@ defmodule Bamboo.SendinBlueAdapter do
   @behaviour Bamboo.Adapter
 
   alias Bamboo.Email
+  alias Bamboo.Attachment
 
   defmodule ApiError do
     defexception [:message]
@@ -37,18 +38,18 @@ defmodule Bamboo.SendinBlueAdapter do
     end
 
     def exception(%{params: params, response: response}) do
-      filtered_params = params |> Plug.Conn.Query.decode |> Map.put("key", "[FILTERED]")
+      filtered_params = params |> Plug.Conn.Query.decode() |> Map.put("key", "[FILTERED]")
 
       message = """
       There was a problem sending the email through the SendinBlue API v2.0.
 
       Here is the response:
 
-      #{inspect response, limit: :infinity}
+      #{inspect(response, limit: :infinity)}
 
       Here are the params we sent:
 
-      #{inspect filtered_params, limit: :infinity}
+      #{inspect(filtered_params, limit: :infinity)}
 
       If you are deploying to Heroku and using ENV variables to handle your API key,
       you will need to explicitly export the variables so they are available at compile time.
@@ -59,20 +60,23 @@ defmodule Bamboo.SendinBlueAdapter do
         SENDINBLUE_API_KEY
       )
       """
+
       %ApiError{message: message}
     end
   end
 
   def deliver(email, config) do
     api_key = get_key(config)
-    body = email |> to_sendinblue_body |> Poison.encode!
+    body = email |> to_sendinblue_body |> Poison.encode!()
     url = [base_uri(), @send_message_path]
 
     case :hackney.post(url, headers(api_key), body, [:with_body]) do
       {:ok, status, _headers, response} when status > 299 ->
         raise(ApiError, %{params: body, response: response})
+
       {:ok, status, headers, response} ->
         %{status_code: status, headers: headers, body: response}
+
       {:error, reason} ->
         raise(ApiError, %{message: inspect(reason)})
     end
@@ -87,6 +91,8 @@ defmodule Bamboo.SendinBlueAdapter do
     end
   end
 
+  def supports_attachments?, do: true
+
   defp get_key(config) do
     case Map.get(config, :api_key) do
       nil -> raise_api_key_error(config)
@@ -100,15 +106,12 @@ defmodule Bamboo.SendinBlueAdapter do
 
     * Here are the config options that were passed in:
 
-    #{inspect config}
+    #{inspect(config)}
     """
   end
 
   defp headers(api_key) do
-    [
-      {"Content-Type", "application/json"},
-      {"api-key", api_key}
-    ]
+    [{"Content-Type", "application/json"}, {"api-key", api_key}]
   end
 
   defp to_sendinblue_body(%Email{} = email) do
@@ -121,32 +124,30 @@ defmodule Bamboo.SendinBlueAdapter do
     |> put_subject(email)
     |> put_html_body(email)
     |> put_text_body(email)
+    |> put_attachments(email)
   end
 
   defp put_from(body, %Email{from: {nil, address}}), do: Map.put(body, :from, [address, ""])
+
   defp put_from(body, %Email{from: {name, address}}) do
     body
     |> Map.put(:from, [address, name])
   end
 
   defp put_to(body, %Email{to: to}) do
-    {names, addresses} = Enum.unzip(to)
-    body
-    |> put_addresses(:to, %{Enum.join(addresses, ",") => Enum.join(names, ",")})
+    body |> put_addresses(:to, address_map(to))
   end
 
   defp put_cc(body, %Email{cc: []}), do: body
+
   defp put_cc(body, %Email{cc: cc}) do
-    {names, addresses} = Enum.unzip(cc)
-    body
-    |> put_addresses(:cc, %{Enum.join(addresses, ",") => Enum.join(names, ",")})
+    body |> put_addresses(:cc, address_map(cc))
   end
 
   defp put_bcc(body, %Email{bcc: []}), do: body
+
   defp put_bcc(body, %Email{bcc: bcc}) do
-    {names, addresses} = Enum.unzip(bcc)
-    body
-    |> put_addresses(:bcc, %{Enum.join(addresses, ",") => Enum.join(names, ",")})
+    body |> put_addresses(:bcc, address_map(bcc))
   end
 
   defp put_subject(body, %Email{subject: subject}), do: Map.put(body, :subject, subject)
@@ -160,16 +161,35 @@ defmodule Bamboo.SendinBlueAdapter do
   defp put_reply_to(body, %Email{headers: %{"reply-to" => reply_to}} = _email) do
     Map.put(body, :replyto, reply_to)
   end
+
   defp put_reply_to(body, _), do: body
 
   defp put_addresses(body, field, addresses), do: Map.put(body, field, addresses)
 
-  # defp list_empty?([]), do: true
-  # defp list_empty?(list) do
-  #   Enum.all?(list, fn(el) -> el == "" || el == nil end)
-  # end
-
   defp base_uri do
     Application.get_env(:bamboo, :sendinblue_base_uri) || @default_base_uri
+  end
+
+  defp put_attachments(body, %Email{attachments: []}), do: body
+
+  defp put_attachments(body, %Email{attachments: atts}) do
+    attachments =
+      atts
+      |> Enum.reduce(%{}, fn attachment, acc ->
+        put_attachment(acc, attachment)
+      end)
+
+    Map.put(body, :attachment, attachments)
+  end
+
+  defp put_attachment(acc, %Attachment{data: data, filename: filename}) do
+    Map.put(acc, filename, Base.encode64(data))
+  end
+
+  defp address_map(addresses) when is_list(addresses) do
+    addresses
+    |> Enum.reduce(%{}, fn {name, address}, acc ->
+      Map.put(acc, address, name)
+    end)
   end
 end
